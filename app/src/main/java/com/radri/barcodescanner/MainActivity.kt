36e6307d2a1994de482236340a86117d76b8d327
Ad <adrienroux94@gmail.com>
2021-10-27 20:38:58 +0200

    Initial commit

---
+++ app/src/main/java/com/radri/barcodescanner/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.radri.barcodescanner
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.radri.barcodescanner.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.scannerButton)
+
+        button.setOnClickListener { // Perform action on click
+            LiveBarcodeScanningActivity.requestStartScanner(this, button, requestPermissionLauncher)
+        }
+    }
+}
